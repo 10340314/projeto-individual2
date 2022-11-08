747de04ed6f66cb580c0486baad70763d7dd9cba
10340314 <victor.diaz@sptech.school>
2022-11-08 15:59:10 -0300

    criado funcao no banco de dados sql

--- tabelas projeto.sql
+++ tabelas projeto.sql
@@ -1,57 +1,3 @@
--- https://stackoverflow.com/questions/18286532/select-if-exist-else-insert
--- https://www.tutorialspoint.com/mysql/mysql_return_statement.htm#:~:text=The%20RETURN%20statement%20in%20MySQL,use%20LEAVE%20instead%20of%20RETURN.
-
-CREATE DATABASE projetoIndividual;
-
-USE projetoIndividual;
-
-CREATE TABLE grupos (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    dataDebut DATE NOT NULL,
-    empresa VARCHAR(45) NOT NULL
-);
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    sobrenome VARCHAR(45) NOT NULL,
-    email VARCHAR(45) NOT NULL,
-    dtNasc DATE NOT NULL,
-    senha VARCHAR(45) NOT NULL,
-    fkGrupoFav INT NOT NULL,
-    FOREIGN KEY (fkGrupoFav) REFERENCES grupos (id)
-);
-
-INSERT INTO grupos (nome, dataDebut, empresa) VALUES
-	('BTS', '2013-06-13', 'HYBE LABELS');
-    
-    
-DELIMITER $
-CREATE PROCEDURE select_or_insert()
-BEGIN
-SET registroEnd = (SELECT * FROM endereco WHERE ocep = cep
-								    AND orua = rua
-                                    AND obairro = bairro
-                                    AND ocidade = cidade
-                                    AND oestado = estado
-                                    AND onum = num);
-                                    
-IF (ISNULL(registroEnd)) THEN
-	INSERT INTO endereco (cep, rua, bairro, cidade, estado, num) VALUES
-		(ocep, orua, obairro, ocidade, oestado, onum);
-	RETURN LAST_INSERT_ID();
-ELSE
-	RETURN registroEnd;
-END IF;
-END $
-DELIMITER ;
-
-
-
-
-
-/*
 CREATE DATABASE projetoIndividual;
 
 USE projetoIndividual;
@@ -91,31 +37,44 @@ CREATE TABLE usuario (
 
 INSERT INTO grupos (nome, dataDebut, empresa) VALUES
 	('BTS', '2013-06-13', 'HYBE LABELS');
-    
+
+SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    {"id": 1
+
+
 DELIMITER $
 CREATE FUNCTION select_or_insert(dcep VARCHAR(8), drua VARCHAR(45), dbairro VARCHAR(45), dcidade VARCHAR(45), destado CHAR(2), dnum INT)
 RETURNS INT
 DETERMINISTIC
 BEGIN
-SET @registroEnd = (SELECT id FROM endereco WHERE cep = dcep
+/*SET @registroEnd := (SELECT id FROM endereco WHERE cep = dcep
 								    AND rua = drua
                                     AND bairro = dbairro
                                     AND cidade = dcidade
                                     AND estado = destado
                                     AND num = dnum);
-                                    
-IF (ISNULL(registroEnd)) THEN
+*/
+IF (ISNULL((SELECT id FROM endereco WHERE cep = dcep
+								    AND rua = drua
+                                    AND bairro = dbairro
+                                    AND cidade = dcidade
+                                    AND estado = destado
+                                    AND num = dnum))) THEN
 	INSERT INTO endereco (cep, rua, bairro, cidade, estado, num) VALUES
 		(dcep, drua, dbairro, dcidade, destado, dnum);
-	RETURN @teste = (SELECT LAST_INSERT_ID());
+	RETURN LAST_INSERT_ID();
 ELSE
-	RETURN @registroEnd;
+	RETURN (SELECT id FROM endereco WHERE cep = dcep
+								    AND rua = drua
+                                    AND bairro = dbairro
+                                    AND cidade = dcidade
+                                    AND estado = destado
+                                    AND num = dnum);
 END IF;
 END $
 DELIMITER ;
 
 drop function select_or_insert;
-
 /*
 esse if n ta funcionando, n sei como usar
 IF (ISNULL(SELECT id FROM endereco WHERE cep = '09570410'
